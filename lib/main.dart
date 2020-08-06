89c309cd673fcf06ff6690ac98317dc0f73094af
rizaljanuari <rizaljanuari4@gmail.com>
2020-08-07 00:08:38 +0700

    upgrading a lil bit

--- lib/main.dart
+++ lib/main.dart
@@ -25,28 +25,45 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void getRandom() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
-              },
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                  onPressed: () {
+                    setState(() => getRandom());
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                  onPressed: () {
+                    setState(() => getRandom());
+                  },
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-              onPressed: () {
-                rightDiceNumber = Random().nextInt(6)+1;
-              },
-            ),
+          SizedBox(
+            height: 160.0,
+          ),
+          RaisedButton(
+            onPressed: () {
+              setState(() => getRandom());
+            },
+            child: Text('Shake the Dice!'),
           ),
         ],
       ),
